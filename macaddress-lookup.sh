aad9a53898478d2c31583250edc2879ee37dd3f7
Charles Mouk <charles.mouk@kroger.com>
2021-03-19 04:45:38 -0500

    declare variables for databases

--- macaddress-lookup.sh
+++ macaddress-lookup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+MACADDRESS_IO_DB="https://macaddress.io/database/macaddress.io-db.json"
+WIRESHARK_MANUF_DB="https://gitlab.com/wireshark/wireshark/-/raw/master/manuf"
+
 DATABASE=https://macaddress.io/database/macaddress.io-db.json
 MAC_ADDR=$1
 OUI=${MAC_ADDR:0:8}
