11930e0e52d9bdd4eef2bc4e25aa3cc2d37b452a
Charles Mouk <charles.mouk@kroger.com>
2021-03-19 05:03:36 -0500

    fix array declaration & expansion

--- macaddress-lookup.sh
+++ macaddress-lookup.sh
@@ -3,9 +3,9 @@
 MACADDRESS_IO_DB="https://macaddress.io/database/macaddress.io-db.json"
 WIRESHARK_MANUF_DB="https://gitlab.com/wireshark/wireshark/-/raw/master/manuf"
 
-DATABASES=($MACADDRESS_IO_DB,$WIRESHARK_MANUF_DB)
+DATABASES=($MACADDRESS_IO_DB $WIRESHARK_MANUF_DB)
 MAC_ADDR=$1
 OUI=${MAC_ADDR:0:8}
-for i in ${DATABASES[@]}; do
+for i in "${DATABASES[@]}"; do
 	curl -s -q $i | grep -wE $OUI
 done
